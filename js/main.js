8a35456ee09c421f93616ea47f4ed231556a830c
Diego <diegocastro1087@gmail.com>
2025-10-21 16:28:25 -0300

    adicionando dashboard

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const veiculos = document.querySelectorAll(".veiculo_dados")
+const quantidade_veiculos = document.getElementById("contagem_veiculos")
+
+quantidade_veiculos.innerHTML += veiculos.length
