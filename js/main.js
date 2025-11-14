0496be450483b7990f1ff7a6085022dad8264deb
Diego Castro <diegocastro1087@gmail.com>
2025-11-14 19:09:10 -0300

    dados deletados

--- js/main.js
+++
@@ -1,4 +0,0 @@
-const veiculos = document.querySelectorAll(".veiculo_dados")
-const quantidade_veiculos = document.getElementById("contagem_veiculos")
-
-quantidade_veiculos.innerHTML += veiculos.length
